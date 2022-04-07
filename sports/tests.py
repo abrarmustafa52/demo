2b74cd14ac48e79e2b024fa8e7ce1b82a18e65ef
Abrar Mustafa <abrarmustafa25@yahoo.com>
2022-04-07 18:24:52 +0500

    (A-2)(A-3)
    just test case to check validity

--- sports/tests.py
+++ sports/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+ 
+
+from .models import sports
+from django.db.models import Count
 
 # Create your tests here.
+class SportsViewTest(TestCase):
+
+    def test_isvalid_nplayers(self):
+        response = self.client.get("/nplayers")
+        self.assertEqual(response.status_code, 200)
+
+    def test_isvalid_noplayers(self):
+        response = self.client.get("/nplayers")
+        self.assertEqual(response.status_code, 200)
